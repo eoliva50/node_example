a5c239a7a54836a4e56f396b0b628a9eedd5efcc
eoliva50 <eoliva50>
2023-10-22 21:32:30 -0500

    Install morgan

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-/* eslint-disable prettier/prettier */
+import './helpers/dotenv'
+
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import logger from './utils/logger'
+import router from './routes'
+import { notFound, errorHandler } from './utils/errors'
+
+const port = Number(process.env.PORT)
 
 const app = express()
-const port = process.env.PORT || 3000
 
-app.get('/', (req,res) => {
-    res.send({msg: "Hello There"})
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
+app.use(helmet())
+
+app.use('/', router)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`)
 })
-
-
-app.listen(port)
