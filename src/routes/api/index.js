06a3acf682bc38954ad926173b5e51fcdbfe85e9
eoliva50 <eoliva50>
2023-11-06 00:48:56 -0600

    Final

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import persons from './persons'
 import companies from './companies'
 import departments from './departments'
+import employees from './employees'
 
 const router = Router()
 
@@ -14,5 +15,6 @@ router.get('/', (req, res) => {
 router.use('/persons', persons)
 router.use('/companies', companies)
 router.use('/departments', departments)
+router.use('/employees', employees)
 
 export default router
