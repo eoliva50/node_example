7fd14c6f98e6ac9ec7a78aee76723445f7e5a4d2
eoliva50 <eoliva50>
2023-11-12 16:41:02 -0600

    Changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-import basicAuth from 'express-basic-auth'
 
 import logger from './helpers/logger'
 import router from './routes'
@@ -15,12 +14,6 @@ import { notFound, errorHandler } from './helpers/errors'
 const port = Number(process.env.PORT)
 
 const app = express()
-
-router.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
-  }),
-)
 
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(
